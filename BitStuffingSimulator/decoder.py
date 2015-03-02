f08253075a3379fe6f0dda058e937652da02a6e4
Arjun Krishna Babu <arjunkbabu@users.noreply.github.com>
2015-03-02 22:31:20 +0530

    Sample decoder implementation. Not yet functional

---
+++ BitStuffingSimulator/decoder.py
@@ -0,0 +1,19 @@
+encoded_bits = raw_input(" Enter encoded bits: ");
+count = 0;
+original_bits = ""
+encoded_bits = encoded_bits[8:-8] #get rid of the header and the tailer
+
+print " After slicing: " + encoded_bits
+
+for x in encoded_bits:
+	if count == 5 :
+		continue;
+	
+	if x == 1 :
+		count = count + 1;
+	else	:
+		count = 0;
+
+	original_bits += x;
+
+print " Original bits: " + original_bits
