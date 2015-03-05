83cbe11b0cc8e2ccf880f0b8c0003ee9513ae8c6
Arjun Krishna Babu <arjunkbabu@users.noreply.github.com>
2015-03-05 17:21:43 +0530

    Improved version which makes use of command-line parameters. NOT fully implemented

---
+++ FileReader_PYTHON/WordReplacer_2.py
@@ -0,0 +1,36 @@
+"""
+	FILE: WordReplacer.py
+
+	Program to open a word, and replace all occurrences of
+	a particular word with another word.
+
+"""
+
+print "Welcome to File Reader";
+print "Enter File Name: ";
+# name = raw_input(" > ");
+name = sys.argv[1];
+
+myfile =  open(name, 'r+'); #open the file for writing
+content = myfile.read(); #store the contents of the file
+print "Original Contents: \n" + content; #print original contents of file
+
+# original_word = raw_input(" Enter word to replace: ");
+# replacement_word = raw_input(" Enter new replacement word: ");
+original_word = sys.argv[2];
+replacement_word = sys.argc[3];
+
+#perform replacement
+newcontent = content.replace(original_word, replacement_word);
+
+myfile.seek(0);		#bring file pointer to the beginning of file,
+					#to replace contents from the beginning
+
+myfile.truncate();	#clear existing contents of file
+
+myfile.write(newcontent); 	#store replaced contents
+
+myfile.seek(0); 	#bring file pointer to the beginning of the file
+print "New Contents: \n" + myfile.read(); #print new contents
+
+myfile.close();
