65f905a6b155fcb449184e3bf5f5b94de7397006
Arjun Krishna Babu <arjunkbabu@users.noreply.github.com>
2015-03-09 22:03:37 +0530

    Working implementation. Still lot more work that needs to be done

--- WebTech_LABASSGN/Question3/AvgMark/myscript.js
+++ WebTech_LABASSGN/Question3/AvgMark/myscript.js
@@ -1,23 +1,40 @@
 function getSubNum()    {
     var num = prompt(" Enter number of subjects: ");
     num = parseInt(num);
+    
+/*
+    var s = num + num;
+    
+    alert("Sum " + s);
+*/
     return num;
 }
 
 function getSubMarks(subnum)    {
-    var mark = new Array();
+    var marks = new Array();
     var sum = 0;
     var average = 0;
+    var elem;
     
     for(var i = 0; i < subnum; i++) {
         subject_mark = prompt("\n Enter marks in subject " + (i+1));    
         marks[i] = parseInt(subject_mark);
+
         sum += marks[i];
+        
+        elem = "mark" + (i+1);
+        
+        document.getElementById(elem).innerHTML = subject_mark;
     }
     
     average = sum/subnum;
     
-    
-        
+    document.getElementById("average_mark").innerHTML = average;    
 
+}
+
+function init() {
+    var x = getSubNum();
+    
+    getSubMarks( parseInt(x) );
 }
